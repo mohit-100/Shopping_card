df943fb9d96c42872f802aa35abb48887385e6f8
mohit <ms7417677@gmail.com>
2023-09-12 14:27:51 +0530

    first commit

---
+++ src/component/Amazon.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import list from "../data";
+import "../styles/Amazon.css"
+import Cards from "./Cards"
+
+
+const Amazon = ({handleClick}) => {
+  return (
+    <section>
+        {
+           list.map((item) => (
+            <Cards item={item} key={item.id} handleClick={handleClick}/>
+        ))
+        
+        }
+    </section>
+  )
+}
+
+export default Amazon
